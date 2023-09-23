fd33b3af89feae0c992ccae7371cd9f5cd4c614f
nikitosik <nikitadanko2002@gmail.com>
2023-09-23 23:53:12 +0300

    add blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import *
+class Blog(models.Model):
+    blog_title = models.CharField(max_length=300)
+    blog_date = models.DateTimeField()
+    blog_text = models.TextField()
+    blog_image=models.ImageField(upload_to='blogs_images/')
 
-# Create your models here.
+
